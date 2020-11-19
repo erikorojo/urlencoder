d0efdd576222c4b753eee485db4dbe261d090aff
super3ggo <14225024+super3ggo@users.noreply.github.com>
2020-11-19 01:59:09 -0600

    Update indentation.

--- bin/test.sh
+++ bin/test.sh
@@ -9,13 +9,13 @@
 ctrl_unencoded=" !@#$%^&*()[]{}/=\?+|',.\"<>"
 ctrl_encoded="%20%21%40%23%24%25%5E%26%2A%28%29%5B%5D%7B%7D%2F%3D%5C%3F%2B%7C%27%2C.%22%3C%3E"
 
-echo "Type in the string: $ctrl_unencoded"
+echo "Type in the string: $ctrl_unencoded (first char is a space)"
 
 c_encoded="$(./bin/urlencode)"
 
 if [ "$ctrl_encoded" = "$c_encoded" ]
 then
-    echo "success"
+  echo "success"
 else
-    echo "failure"
+  echo "failure"
 fi
